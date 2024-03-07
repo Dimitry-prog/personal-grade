cc310b71609db6c9d0b05ce6c0c2a3071d0483a8
Dmitry <kfifa3@gmail.com>
2024-03-07 20:33:33 +0300

    fix: logo link

--- components/shared/logo.tsx
+++ components/shared/logo.tsx
@@ -1,16 +1,19 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Logo = () => {
   return (
     <div className="w-60 md:w-80">
-      <Image
-        src="/icons/logo-white.svg"
-        alt="logo"
-        width={100}
-        height={32}
-        className="w-full object-cover"
-        loading="lazy"
-      />
+      <Link href="/">
+        <Image
+          src="/icons/logo-white.svg"
+          alt="logo"
+          width={100}
+          height={32}
+          className="w-full object-cover"
+          loading="lazy"
+        />
+      </Link>
     </div>
   );
 };
